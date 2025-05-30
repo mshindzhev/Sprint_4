8579ecf4d14a460e87adadbdd0fa63ec4558e3d2
mshindzhev <shindzhev@gmail.com>
2025-05-30 10:12:14 +0300

    fixed first test

--- tests/test_BooksCollector.py
+++ tests/test_BooksCollector.py
@@ -7,7 +7,7 @@ class TestBooksCollector:
 
     def test_add_new_book_with_name_more_41_symbol_book_not_added(self, collector):
         collector.add_new_book(name='Колобок и его друзья с кучей символов пришли сломать нам тест')
-        assert collector.get_book_genre(name='Колобок') is None
+        assert collector.get_book_genre(name='Колобок и его друзья с кучей символов пришли сломать нам тест') is None
 
 
 
