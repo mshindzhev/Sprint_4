1bf0865797c8e76f2fd65efe112f9dfc38cbfbea
mshindzhev <shindzhev@gmail.com>
2025-05-30 10:10:47 +0300

    added first test

--- tests/test_BooksCollector.py
+++ tests/test_BooksCollector.py
@@ -1,9 +1,13 @@
 
 class TestBooksCollector:
 
-    def test_add_new_book(self, collector):
+    def test_add_new_book_with_name_less_41_symbol_book_added(self, collector):
         collector.add_new_book(name='Колобок')
         assert collector.get_book_genre(name='Колобок') is not None
+
+    def test_add_new_book_with_name_more_41_symbol_book_not_added(self, collector):
+        collector.add_new_book(name='Колобок и его друзья с кучей символов пришли сломать нам тест')
+        assert collector.get_book_genre(name='Колобок') is None
 
 
 
